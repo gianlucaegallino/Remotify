a83ae7e5df66aa22176e381c364a17453a3ee5fa
Gianluca <gianlucaegallino@gmail.com>
2024-03-04 19:55:52 -0300

    Added state logging for song titles and artists

--- scripts/connect.js
+++ scripts/connect.js
@@ -1,55 +1,75 @@
 //Creates a new spotify sdk player object
 
 window.onSpotifyWebPlaybackSDKReady = () => {
-  const token = 'BQCBhgONYE4b9loWhJ6rDJbmJzG6jF6ZtCuZfnlcfIgm26c6ku8vi7LcqsfpTqzwfpnEyTMOlk1E9VQTdiSuWIjFhjpzhopiroq2Gfswsj3szq5vdWuupHsDQ3S31atXB-krmARKiGJQnzYe3xsz7H8VqOSgQXcMPse9uDemD6ncc-ZQGzDhZViyzT0WQ2O1qff_v0KCRrU  ';
+  const token =
+    "BQA5jt4Sr1hoh_lsPp4klpHgrYKNjE--HHQjFwWKtJn0ZSt2xxhvGrDOvrq9JH9ElQosWXHZN1GfvbydcMem_m8ejoI-VmYdom91IOvbvnfCybX0cnofdzsSBVTJhSZB7tNYPBi06CxdUdA6xrrdV4ZbVJGLiiQ6wQYmyiOF4G_WdWIzdHYPDMa258dVXdQ3-wFe_PmVz0g";
   const player = new Spotify.Player({
-      name: 'Remotify Player',
-      getOAuthToken: cb => { cb(token); },
-      volume: 0.5
+    name: "Remotify Player",
+    getOAuthToken: (cb) => {
+      cb(token);
+    },
+    volume: 0.5,
   });
 
   //Status logging
   // Ready
-  player.addListener('ready', ({ device_id }) => {
-      console.log('Ready with Device ID', device_id);
+  player.addListener("ready", ({ device_id }) => {
+    console.log("Ready with Device ID", device_id);
   });
 
   // Not Ready
-  player.addListener('not_ready', ({ device_id }) => {
-      console.log('Device ID has gone offline', device_id);
+  player.addListener("not_ready", ({ device_id }) => {
+    console.log("Device ID has gone offline", device_id);
   });
 
   // Error logging listeners
 
-  player.addListener('initialization_error', ({ message }) => {
-      console.error(message);
+  player.addListener("initialization_error", ({ message }) => {
+    console.error(message);
   });
 
-  player.addListener('authentication_error', ({ message }) => {
-      console.error(message);
+  player.addListener("authentication_error", ({ message }) => {
+    console.error(message);
   });
 
-  player.addListener('account_error', ({ message }) => {
-      console.error(message);
+  player.addListener("account_error", ({ message }) => {
+    console.error(message);
   });
 
   // Button listeners
 
-  document.getElementById('togglePlay').onclick = function() {
+  document.getElementById("togglePlay").onclick = function () {
     player.togglePlay();
   };
 
-  document.getElementById('previousTrack').onclick = function() {
+  document.getElementById("previousTrack").onclick = function () {
     player.previousTrack();
   };
 
-  document.getElementById('player.nextTrack').onclick = function() {
+  document.getElementById("nextTrack").onclick = function () {
     player.nextTrack();
   };
 
+  // Connection state listeners
+
+  
+
+  player.addListener(
+    "player_state_changed",
+    ({ track_window: { current_track } }) => {
+      let returnedplaying = `Currently Playing: ${current_track.name}`
+      let returnedartists = `Artists: ${(() => {
+        artists = []
+        for (const artist of current_track.artists) {
+          artists.push(artist.name)
+        }
+        return artists.join(", ");
+      })()}`
+      document.getElementById("currentlyPlaying").innerHTML = returnedplaying
+      document.getElementById("artists").innerHTML = returnedartists
+    }
+  );
+
   // connects the player
   player.connect();
-
-  //testing, should keep the playing status
-  player.activateElement();
-}
+};
