71c03f6d289eed1a9850c36f49aa064c86a52f92
Gianluca <gianlucaegallino@gmail.com>
2024-03-14 11:35:49 -0300

    Fix login and add logout + refresh token support

--- scripts/connect.js
+++ scripts/connect.js
@@ -1,66 +1,76 @@
 /**
  * This is an example of a basic node.js script that performs
- * the Authorization Code with PKCE oAuth2 flow to authenticate 
+ * the Authorization Code with PKCE oAuth2 flow to authenticate
  * against the Spotify Accounts.
  *
  * For more information, read
  * https://developer.spotify.com/documentation/web-api/tutorials/code-pkce-flow
  */
 
-const clientId = '60f8dab9ab5f46ab993a5378bea82f26'; // your clientId
-const redirectUrl = 'chrome-extension://hndbihmidcdkhcpbjodeagbolmbbmolj/popup/remotify.html'; // your redirect URL - must be localhost URL and/or HTTPS
+const clientId = "60f8dab9ab5f46ab993a5378bea82f26"; // your clientId
+const redirectUrl =
+  "chrome-extension://hndbihmidcdkhcpbjodeagbolmbbmolj/popup/remotify.html"; // your redirect URL - must be localhost URL and/or HTTPS
 
 const authorizationEndpoint = "https://accounts.spotify.com/authorize";
 const tokenEndpoint = "https://accounts.spotify.com/api/token";
-const scope = 'user-read-private user-read-email';
+const scope = "user-read-private user-read-email";
 
 // Data structure that manages the current active token, caching it in localStorage
 const currentToken = {
-  get access_token() { return localStorage.getItem('access_token') || null; },
-  get refresh_token() { return localStorage.getItem('refresh_token') || null; },
-  get expires_in() { return localStorage.getItem('refresh_in') || null; },
-  get expires() { return localStorage.getItem('expires') || null; },
+  get access_token() {
+    return localStorage.getItem("access_token") || null;
+  },
+  get refresh_token() {
+    return localStorage.getItem("refresh_token") || null;
+  },
+  get expires_in() {
+    return localStorage.getItem("refresh_in") || null;
+  },
+  get expires() {
+    return localStorage.getItem("expires") || null;
+  },
 
   save: function (response) {
     const { access_token, refresh_token, expires_in } = response;
-    localStorage.setItem('access_token', access_token);
-    localStorage.setItem('refresh_token', refresh_token);
-    localStorage.setItem('expires_in', expires_in);
+    localStorage.setItem("access_token", access_token);
+    localStorage.setItem("refresh_token", refresh_token);
+    localStorage.setItem("expires_in", expires_in);
 
     const now = new Date();
-    const expiry = new Date(now.getTime() + (expires_in * 1000));
-    localStorage.setItem('expires', expiry);
-  }
+    const expiry = new Date(now.getTime() + expires_in * 1000);
+    localStorage.setItem("expires", expiry);
+  },
 };
 
 // On page load, try to fetch auth code from current browser search URL
 const args = new URLSearchParams(window.location.search);
-let code = args.get('code');
+let code = args.get("code");
 
 // If we find a code, we're in a callback, do a token exchange
 if (code) {
   getToken(code)
-    .then(token => {
+    .then((token) => {
       currentToken.save(token);
 
       // Remove code from URL so we can refresh correctly.
       const url = new URL(window.location.href);
       url.searchParams.delete("code");
 
-      const updatedUrl = url.search ? url.href : url.href.replace('?', '');
+      const updatedUrl = url.search ? url.href : url.href.replace("?", "");
       window.history.replaceState({}, document.title, updatedUrl);
+      window.location.reload();
     })
-    .catch(error => console.error("Error getting token:", error));
+    .catch((error) => console.error("Error getting token:", error));
 }
 
 // If we have a token, we're logged in, so fetch user data and render logged-in template
 if (currentToken.access_token) {
   getUserData()
-    .then(userData => {
+    .then((userData) => {
       renderTemplate("main", "logged-in-template", userData);
       renderTemplate("oauth", "oauth-template", currentToken);
     })
-    .catch(error => console.error("Error fetching user data:", error));
+    .catch((error) => console.error("Error fetching user data:", error));
 }
 
 // Otherwise, we're not logged in, so render the login template
@@ -69,28 +79,34 @@ if (!currentToken.access_token) {
 }
 
 async function redirectToSpotifyAuthorize() {
-  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   const randomValues = crypto.getRandomValues(new Uint8Array(64));
-  const randomString = randomValues.reduce((acc, x) => acc + possible[x % possible.length], "");
+  const randomString = randomValues.reduce(
+    (acc, x) => acc + possible[x % possible.length],
+    ""
+  );
 
   const code_verifier = randomString;
   const data = new TextEncoder().encode(code_verifier);
-  const hashed = await crypto.subtle.digest('SHA-256', data);
+  const hashed = await crypto.subtle.digest("SHA-256", data);
 
-  const code_challenge_base64 = btoa(String.fromCharCode(...new Uint8Array(hashed)))
-    .replace(/=/g, '')
-    .replace(/\+/g, '-')
-    .replace(/\//g, '_');
+  const code_challenge_base64 = btoa(
+    String.fromCharCode(...new Uint8Array(hashed))
+  )
+    .replace(/=/g, "")
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_");
 
-  window.localStorage.setItem('code_verifier', code_verifier);
+  window.localStorage.setItem("code_verifier", code_verifier);
 
   const authUrl = new URL(authorizationEndpoint);
   const params = {
-    response_type: 'code',
+    response_type: "code",
     client_id: clientId,
     scope: scope,
     //TODO: add state
-    code_challenge_method: 'S256',
+    code_challenge_method: "S256",
     code_challenge: code_challenge_base64,
     redirect_uri: redirectUrl,
   };
@@ -101,16 +117,16 @@ async function redirectToSpotifyAuthorize() {
 
 // Spotify API Calls
 async function getToken(code) {
-  const code_verifier = localStorage.getItem('code_verifier');
+  const code_verifier = localStorage.getItem("code_verifier");
 
   const response = await fetch(tokenEndpoint, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
+      "Content-Type": "application/x-www-form-urlencoded",
     },
     body: new URLSearchParams({
       client_id: clientId,
-      grant_type: 'authorization_code',
+      grant_type: "authorization_code",
       code: code,
       redirect_uri: redirectUrl,
       code_verifier: code_verifier,
@@ -122,14 +138,14 @@ async function getToken(code) {
 
 async function refreshToken() {
   const response = await fetch(tokenEndpoint, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
+      "Content-Type": "application/x-www-form-urlencoded",
     },
     body: new URLSearchParams({
       client_id: clientId,
-      grant_type: 'refresh_token',
-      refresh_token: currentToken.refresh_token
+      grant_type: "refresh_token",
+      refresh_token: currentToken.refresh_token,
     }),
   });
 
@@ -138,8 +154,8 @@ async function refreshToken() {
 
 async function getUserData() {
   const response = await fetch("https://api.spotify.com/v1/me", {
-    method: 'GET',
-    headers: { 'Authorization': 'Bearer ' + currentToken.access_token },
+    method: "GET",
+    headers: { Authorization: "Bearer " + currentToken.access_token },
   });
 
   return await response.json();
@@ -148,30 +164,37 @@ async function getUserData() {
 // Click handlers
 async function loginWithSpotifyClick() {
   await redirectToSpotifyAuthorize();
+  
 }
 
 async function logoutClick() {
+  console.log("deleting stuff");
   localStorage.clear();
   window.location.href = redirectUrl;
 }
 
 async function refreshTokenClick() {
+  console.log("refreshing token");
   const token = await refreshToken();
   currentToken.save(token);
   renderTemplate("oauth", "oauth-template", currentToken);
 }
 
-// HTML Template Rendering with basic data binding - demoware only.
+// HTML Template Rendering 
 function renderTemplate(targetId, templateId, data = null) {
   const template = document.getElementById(templateId);
   const clone = template.content.cloneNode(true);
 
   const elements = clone.querySelectorAll("*");
-  elements.forEach(ele => {
-    const bindingAttrs = [...ele.attributes].filter(a => a.name.startsWith("data-bind"));
+  elements.forEach((ele) => {
+    const bindingAttrs = [...ele.attributes].filter((a) =>
+      a.name.startsWith("data-bind")
+    );
 
-    bindingAttrs.forEach(attr => {
-      const target = attr.name.replace(/data-bind-/, "").replace(/data-bind/, "");
+    bindingAttrs.forEach((attr) => {
+      const target = attr.name
+        .replace(/data-bind-/, "")
+        .replace(/data-bind/, "");
       const targetType = target.startsWith("onclick") ? "HANDLER" : "PROPERTY";
       const targetProp = target === "" ? "innerHTML" : target;
 
@@ -181,12 +204,12 @@ function renderTemplate(targetId, templateId, data = null) {
       // Maybe use a framework with more validation here ;)
       try {
         if (targetType === "PROPERTY") {
-          ele[targetProp] = data ? data[attr.value] : '';
+          ele[targetProp] = data ? data[attr.value] : "";
         } else {
           // Event handler binding
-          ele.addEventListener(targetProp.replace('on', ''), () => {
+          ele.addEventListener(targetProp.replace("on", ""), () => {
             const handlerName = attr.value;
-            if (typeof window[handlerName] === 'function') {
+            if (typeof window[handlerName] === "function") {
               window[handlerName]();
             }
           });
@@ -201,14 +224,41 @@ function renderTemplate(targetId, templateId, data = null) {
   const target = document.getElementById(targetId);
   target.innerHTML = "";
   target.appendChild(clone);
+
+  addPageListeners();
 }
 
-document.getElementById("printAuth").addEventListener('click', function () {
+document.getElementById("printAuth").addEventListener("click", function () {
   // Retrieve the access token from local storage
   const accessToken = localStorage.getItem("access_token");
   console.log(accessToken);
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("login-button").addEventListener("click", loginWithSpotifyClick);
-});
+function addPageListeners() {
+  const loginButton = document.getElementById("login-button");
+  if (loginButton) {
+    loginButton.addEventListener("click", loginWithSpotifyClick);
+    console.log("binded to 1");
+  }
+  else{
+    console.log("no login here");
+  }
+
+  const refreshButton = document.getElementById("refresh-token-button");
+  if (refreshButton) {
+    refreshButton.addEventListener("click", refreshTokenClick);
+    console.log("binded to 2");
+  }
+  else{
+    console.log("no refresh here");
+  }
+
+  const logoutButton = document.getElementById("logout-button");
+  if (logoutButton) {
+    logoutButton.addEventListener("click", logoutClick);
+    console.log("binded to 3");
+  }
+  else{
+    console.log("no logout here");
+  }
+};
